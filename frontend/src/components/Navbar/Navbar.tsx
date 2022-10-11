b8bb6e1dc5a486eeeef3744b70988bac6030a1f9
Eva Teisberg <evatei@stud.ntnu.no>
2022-10-11 12:11:07 +0200

    feat: navbar

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+export default function Navbar() {
+  return <div className='navbar'>
+    <a href="/">
+      <img
+        className="logo"
+        src={require("../../assets/logo.png")}
+        alt=""
+      />
+    </a>
+  </div>;
+}
