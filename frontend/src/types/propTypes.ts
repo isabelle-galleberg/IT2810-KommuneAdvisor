d2ba961b8d2b814c03fb4a425f5c29fc2b12d199
evateis <evateisb@gmail.com>
2022-10-13 14:59:46 +0200

    feat: input fields for filtering by county and sorting

--- frontend/src/types/propTypes.ts
+++ frontend/src/types/propTypes.ts
@@ -3,3 +3,13 @@ export interface KommuneCardProps {
   county: string;
   weapon: string;
 }
+
+export interface InputFieldsProps {
+  setParameters: (parameters: { county: string, sort: string }) => void;
+  parameters: { county: string, sort: string };
+  setCountys: (countys: string[]) => void;
+  countys: string[];
+  setSort: (sort: string) => void;
+  sort: string;
+}
+
