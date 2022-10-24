49a3e97ce88e76f44428769fde0b71652e567ff6
evateis <evateisb@gmail.com>
2022-10-24 21:45:49 +0200

    test: cypress

--- frontend/cypress/integration/test-app.spec.ts
+++ frontend/cypress/integration/test-app.spec.ts
@@ -5,7 +5,16 @@ describe('test the home screen of KommuneAdvisor', () => {
 
   it('should display main page when opening for the first time', () => {
     cy.get('.logo').should('exist');
-    cy.get('.mantine-i0taq').should('exist');
-    cy.contains('.mantine-1m3pqry', 'Søk etter kommune')
+    cy.contains('.mantine-1m3pqry', 'Søk etter kommune');
+    cy.contains('.mantine-1m3pqry', 'Filtrer på fylke');
   })
+
+  it('test routning', () => {
+    cy.contains('button', 'Vis mer').click();
+    cy.get('.backArrow').click();
+    cy.contains('button', 'Vis mer').click();
+    cy.get('.logo').click();
+    cy.contains('.mantine-1m3pqry', 'Søk etter kommune');
+  })
+
 })
