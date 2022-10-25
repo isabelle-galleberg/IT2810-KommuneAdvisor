b11dbd02ae988e7bd8706f17b339880a99092a5f
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-25 13:22:27 +0200

    fix: fetch kommuner on name instead of id

--- backend/server/schema/schema.js
+++ backend/server/schema/schema.js
@@ -1,138 +1,139 @@
 const {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLList,
-  GraphQLSchema,
-  GraphQLFloat,
-  GraphQLNonNull,
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList,
+	GraphQLSchema,
+	GraphQLEnumType,
+	GraphQLFloat,
+	GraphQLNonNull,
 } = require("graphql");
 
 const kommuner = require("../models/kommune");
 const kommuneRating = require("../models/kommuneRating");
 
 const KommuneType = new GraphQLObjectType({
-  name: "Kommune",
-  fields: () => ({
-    _id: { type: GraphQLID },
-    kommuneNumber: { type: GraphQLString },
-    name: { type: GraphQLString },
-    population: { type: GraphQLInt },
-    areaInSquareKm: { type: GraphQLFloat },
-    landAreaInSquareKm: { type: GraphQLFloat },
-    populationByArea: { type: GraphQLFloat },
-    mapUrl: { type: GraphQLString },
-    logoUrl: { type: GraphQLString },
-    writtenLanguage: { type: GraphQLString },
-    kommuneRating: {
-      type: new GraphQLList(KommuneRatingType),
-      resolve(parent, args) {
-        return kommuneRating.find({ kommuneId: parent._id });
-      },
-    },
-  }),
+	name: "Kommune",
+	fields: () => ({
+		_id: { type: GraphQLID },
+		kommuneNumber: { type: GraphQLString },
+		name: { type: GraphQLString },
+		population: { type: GraphQLInt },
+		areaInSquareKm: { type: GraphQLFloat },
+		landAreaInSquareKm: { type: GraphQLFloat },
+		populationByArea: { type: GraphQLFloat },
+		mapUrl: { type: GraphQLString },
+		logoUrl: { type: GraphQLString },
+		writtenLanguage: { type: GraphQLString },
+		kommuneRating: {
+			type: new GraphQLList(KommuneRatingType),
+			resolve(parent, args) {
+				return kommuneRating.find({ kommuneId: parent._id });
+			},
+		},
+	}),
 });
 
 const KommuneRatingType = new GraphQLObjectType({
-  name: "KommuneRating",
-  fields: () => ({
-    _id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    rating: { type: GraphQLInt },
-    title: { type: GraphQLString },
-    description: { type: GraphQLString },
-    timestamp: { type: GraphQLString },
-    kommuneNumber: { type: GraphQLString },
-  }),
+	name: "KommuneRating",
+	fields: () => ({
+		_id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		rating: { type: GraphQLInt },
+		title: { type: GraphQLString },
+		description: { type: GraphQLString },
+		timestamp: { type: GraphQLString },
+		kommuneNumber: { type: GraphQLString },
+	}),
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    kommuner: {
-      type: new GraphQLList(KommuneType),
-      args: {
-        sortBy: {
-          type: new GraphQLEnumType({
-            name: "sort",
-            values: {
-              name: { value: "name" },
-              population: { value: "population" },
-              area: { value: "area" },
-            },
-            defaultValue: "name",
-          }),
-        },
-        sortDirection: {
-          type: new GraphQLEnumType({
-            name: "sortDirection",
-            values: {
-              ascending: { value: "ascending" },
-              descending: { value: "descending" },
-            },
-            defaultValue: "ascending",
-          }),
-        },
-        page: { type: GraphQLFloat, defaultValue: 1 },
-        pageSize: { type: GraphQLFloat, defaultValue: 10 },
-        search: { type: GraphQLString },
-        county: { type: GraphQLString },
-      },
-      resolve(parent, args) {
-        let query = kommuner.find({});
-        if (args.search)
-          query = query.find({
-            name: { $regex: args.search, $options: "i" },
-          });
-        if (args.sortBy) {
-          query = query.sort({
-            [args.sortBy]: args.sortDirection === "descending" ? -1 : 1,
-          });
-        }
-        query.skip((args.page - 1) * args.pageSize).limit(args.pageSize);
-        return query;
-      },
-    },
-    kommune: {
-      type: new GraphQLList(KommuneType),
-      args: { kommuneNumber: { type: GraphQLString } },
-      resolve(parent, args) {
-        return kommuner.find({ kommuneNumber: args.kommuneNumber });
-      },
-    },
-  },
+	name: "RootQueryType",
+	fields: {
+		kommuner: {
+			type: new GraphQLList(KommuneType),
+			args: {
+				sortBy: {
+					type: new GraphQLEnumType({
+						name: "sort",
+						values: {
+							name: { value: "name" },
+							population: { value: "population" },
+							area: { value: "area" },
+						},
+						defaultValue: "name",
+					}),
+				},
+				sortDirection: {
+					type: new GraphQLEnumType({
+						name: "sortDirection",
+						values: {
+							ascending: { value: "ascending" },
+							descending: { value: "descending" },
+						},
+						defaultValue: "ascending",
+					}),
+				},
+				page: { type: GraphQLFloat, defaultValue: 1 },
+				pageSize: { type: GraphQLFloat, defaultValue: 10 },
+				search: { type: GraphQLString },
+				county: { type: GraphQLString },
+			},
+			resolve(parent, args) {
+				let query = kommuner.find({});
+				if (args.search)
+					query = query.find({
+						name: { $regex: args.search, $options: "i" },
+					});
+				if (args.sortBy) {
+					query = query.sort({
+						[args.sortBy]: args.sortDirection === "descending" ? -1 : 1,
+					});
+				}
+				query.skip((args.page - 1) * args.pageSize).limit(args.pageSize);
+				return query;
+			},
+		},
+		kommune: {
+			type: new GraphQLList(KommuneType),
+			args: { kommuneName: { type: GraphQLString } },
+			resolve(parent, args) {
+				return kommuner.find({ name: args.kommuneName.replace("_", " ") });
+			},
+		},
+	},
 });
 
 const RootMutation = new GraphQLObjectType({
-  name: "RootMutationType",
-  fields: {
-    addKommuneRating: {
-      type: KommuneRatingType,
-      args: {
-        name: { type: GraphQLNonNull(GraphQLString) },
-        rating: { type: GraphQLInt }, 
-        title: { type: GraphQLNonNull(GraphQLString) },
-        description: { type: GraphQLNonNull(GraphQLString) },
-        kommuneId: { type: GraphQLNonNull(GraphQLID) },
-      },
-      resolve(parent, args) {
-        const { name, rating, title, description, kommuneId } = args;
-        const newKommuneRating = new kommuneRating({
-          name,
-          rating,
-          title,
-          description,
-          kommuneId,
-          timestamp: new Date(),
-        });
-        return newKommuneRating.save();
-      },
-    },
-  },
+	name: "RootMutationType",
+	fields: {
+		addKommuneRating: {
+			type: KommuneRatingType,
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				rating: { type: GraphQLInt },
+				title: { type: GraphQLNonNull(GraphQLString) },
+				description: { type: GraphQLNonNull(GraphQLString) },
+				kommuneId: { type: GraphQLNonNull(GraphQLID) },
+			},
+			resolve(parent, args) {
+				const { name, rating, title, description, kommuneId } = args;
+				const newKommuneRating = new kommuneRating({
+					name,
+					rating,
+					title,
+					description,
+					kommuneId,
+					timestamp: new Date(),
+				});
+				return newKommuneRating.save();
+			},
+		},
+	},
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation: RootMutation,
+	query: RootQuery,
+	mutation: RootMutation,
 });
