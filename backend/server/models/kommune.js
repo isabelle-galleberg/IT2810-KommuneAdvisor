80d9f108b19e204c7c19dad2a89a5c8524f48246
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-27 14:50:31 +0200

    fix: remove county number

--- backend/server/models/kommune.js
+++ backend/server/models/kommune.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 
 const KommuneSchema = new mongoose.Schema(
-  {
-    kommuneNumber: { type: String },
-    name: { type: String },
-    population: { type: Number },
-    areaInSquareKm: { type: Number },
-    landAreaInSquareKm: { type: Number },
-    populationByArea: { type: Number },
-    mapUrl: { type: String },
-    logoUrl: { type: String },
-    writtenLanguage: { type: String },
-    countyNumber: { type: String },
-    kommuneRating: [
-      { type: mongoose.Schema.Types.String, ref: "kommuneRating" },
-    ],
-    county: { type: mongoose.Schema.Types.Number },
-  },
-  { collection: "kommuner" }
+	{
+		kommuneNumber: { type: String },
+		name: { type: String },
+		population: { type: Number },
+		areaInSquareKm: { type: Number },
+		landAreaInSquareKm: { type: Number },
+		populationByArea: { type: Number },
+		mapUrl: { type: String },
+		logoUrl: { type: String },
+		writtenLanguage: { type: String },
+		kommuneRating: [
+			{ type: mongoose.Schema.Types.String, ref: "kommuneRating" },
+		],
+		county: { type: mongoose.Schema.Types.Number },
+	},
+	{ collection: "kommuner" }
 );
 
 module.exports = mongoose.model("Kommuner", KommuneSchema);
