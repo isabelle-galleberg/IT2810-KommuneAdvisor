e0a40d0121fdef710f13f1d254dc15545a88de89
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-27 10:43:58 +0200

    feat: service function for fetching kommuner and kommune details

--- frontend/src/services/kommuneService.ts
+++ frontend/src/services/kommuneService.ts
@@ -1,31 +1,32 @@
 import { gql } from '@apollo/client/core';
 
-export const GET_KOMMUNE = (kommuneNr: string) => {
-  return gql`
-        query {
-            kommune(kommuneNumber: "${kommuneNr}") {
-                kommuneNumber
-                name
-                population
-                areaInSquareKm
-                mapUrl
-                logoUrl
-                writtenLanguage
-                kommuneRating {
-                    name
-                    rating
-                    title
-                    description
-                    timestamp
-                }
-            }
-        }
-    `;
-};
+export const GET_KOMMUNE = gql`
+  query Kommune($kommuneName: String) {
+    kommune(kommuneName: $kommuneName) {
+      name
+      population
+      areaInSquareKm
+      mapUrl
+      logoUrl
+      writtenLanguage
+      kommuneRating {
+        name
+        rating
+        title
+        description
+        timestamp
+      }
+    }
+  }
+`;
 
 export const GET_ALL_KOMMUNER = gql`
-  query {
-    kommuner {
+  query Kommuner($sortBy: sort, $sortDirection: sortDirection, $pageSize: Int) {
+    kommuner(
+      sortBy: $sortBy
+      sortDirection: $sortDirection
+      pageSize: $pageSize
+    ) {
       name
       logoUrl
       kommuneRating {
