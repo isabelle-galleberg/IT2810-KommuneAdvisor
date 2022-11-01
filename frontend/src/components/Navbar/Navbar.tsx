dc487bff82722b224d75a39c345cb27cd1813d3b
Ole Marius Inderhaug <ole.inderhaug@visma.com>
2022-11-01 23:58:27 +0100

    fix: broken link on logo

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <a href='/'>
+      <a href='/kommuneadvisor'>
         <img
           className='logo'
           src={require('../../assets/logo.png')}
